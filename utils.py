30d0e7cee0d56550b7eb32e65b9ecb0ec03925a7
stepanowital <stepanowital@gmail.com>
2022-10-02 00:45:51 +0600

    step 0 /tests

---
+++ utils.py
@@ -0,0 +1,64 @@
+import json
+
+
+def get_posts_all():
+	with open("data/posts.json", "r", encoding="utf-8") as file:
+		posts = json.load(file)
+		return posts
+
+
+def get_posts_by_user(user_name):
+	user_post = []
+	posts = get_posts_all()
+	for post in posts:
+		if post["poster_name"] == user_name:
+			user_post = post
+	if not user_post:
+		raise ValueError("Такого пользователя не существует")
+	return user_post
+
+
+# print(get_posts_by_user("le"))
+
+
+def get_comments_by_post_id(post_id):
+	posts = get_posts_all()
+	is_post_real = False
+	for post in posts:
+		if post["pk"] == post_id:
+			is_post_real = True
+	if not is_post_real:
+		raise ValueError("Такого поста не существует")
+
+	with open("data/comments.json", "r", encoding="utf-8") as file:
+		comments = json.load(file)
+	comments_by_post_id = []
+	for comment in comments:
+		if comment["post_id"] == post_id:
+			comments_by_post_id.append(comment)
+	return comments_by_post_id
+
+
+# print(get_comments_by_post_id(8))
+
+
+def search_for_posts(query):
+	posts = get_posts_all()
+	posts_query = []
+	for post in posts:
+		if query.lower() in post["content"].lower():
+			posts_query.append(post)
+	return posts_query
+
+
+# print(search_for_posts(""))
+
+
+def get_post_by_pk(pk):
+	posts = get_posts_all()
+	for post in posts:
+		if post["pk"] == pk:
+			return post
+
+
+# print(get_post_by_pk(8))
